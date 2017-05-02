adf3664224e755b4699360176e6f045b1edda97f
guzewski207550 <207550@edu.p.lodz.pl>
2017-05-02 19:18:45 +0200

    Add missing budgetService implementation. (#10)

--- budget-application/src/main/java/com/example/java/application/services/BudgetService.java
+++ budget-application/src/main/java/com/example/java/application/services/BudgetService.java
@@ -4,14 +4,25 @@ import java.util.List;
 import java.util.UUID;
 
 import com.example.java.domain.model.Budget;
+import com.example.java.domain.model.Expense;
+import com.example.java.domain.model.Income;
 
 public interface BudgetService {
 
-	public Budget getOneBudgetEntity(UUID budgetId);
+    public Budget getOneBudgetEntity(UUID budgetId);
 
-	public Budget saveBudgetEntity(Budget budget);
 
-	public Budget getOneByUserIdAndOwner(UUID userID);
+    public Budget saveBudgetEntity(Budget budget);
 
-	public List<Budget> getSharedBudgets(UUID userID);
+
+    public List<Budget> getOneByUserIdAndOwner(UUID userID);
+
+
+    public List<Budget> getSharedBudgets(UUID userID);
+
+
+    public Budget addNewIncome(UUID budgetId, Income income);
+
+
+    public Budget addNewExpense(UUID budgetId, Expense expense);
 }
