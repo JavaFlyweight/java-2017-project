fd10a19af6fb30290cbdebf4a4c42b1c008a5528
zielinski207623 <207623@edu.p.lodz.pl>
2017-05-03 22:26:26 +0200

    implementation getAllSumsIncomesPerType method

--- budget-application/src/main/java/com/example/java/application/services/ReportService.java
+++ budget-application/src/main/java/com/example/java/application/services/ReportService.java
@@ -5,9 +5,12 @@ import java.util.Map;
 import java.util.UUID;
 
 import com.example.java.commons.enums.ExpenseType;
+import com.example.java.commons.enums.IncomeType;
 
 public interface ReportService {
 
 	public Map<ExpenseType, Double> getAllSumsExpensesPerType(UUID budgetId, Date dateFrom, Date dateTo);
+	
+	public Map<IncomeType, Double> getAllSumsIncomesPerType(UUID budgetId, Date dateFrom, Date dateTo);
 
 }
--- budget-application/src/main/java/com/example/java/application/services/ReportServiceImpl.java
+++ budget-application/src/main/java/com/example/java/application/services/ReportServiceImpl.java
@@ -16,9 +16,11 @@ import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Service;
 
 import com.example.java.commons.enums.ExpenseType;
+import com.example.java.commons.enums.IncomeType;
 import com.example.java.commons.enums.PermissionType;
 import com.example.java.commons.exception.BudgetForbiddenAccessException;
 import com.example.java.domain.model.Expense;
+import com.example.java.domain.model.Income;
 import com.example.java.repository.BudgetRepository;
 
 import static com.example.java.application.services.UtilsService.checkPermissionForBudget;
@@ -37,6 +39,7 @@ public class ReportServiceImpl implements ReportService {
 	@Override
 	public Map<ExpenseType, Double> getAllSumsExpensesPerType(UUID budgetId, Date dateFrom, Date dateTo) {
 		Map<ExpenseType, Double> allSumsExpensesPerType = new HashMap<ExpenseType, Double>();
+		LOGGER.debug("Log user is  {0}", new Object[] { userService.getLoggedUserName() });
 
 		if (!checkPermissionForBudget(budgetRepository.findOneById(budgetId), userService.getLoggedUserName(),
 				PermissionType.VIEW)) {
@@ -54,7 +57,29 @@ public class ReportServiceImpl implements ReportService {
 			}
 			return allSumsExpensesPerType;
 		}
+	}
 
+	@Override
+	public Map<IncomeType, Double> getAllSumsIncomesPerType(UUID budgetId, Date dateFrom, Date dateTo) {
+		Map<IncomeType, Double> allSumsIncomesPerType = new HashMap<IncomeType, Double>();
+		LOGGER.debug("Log user is  {0}", new Object[] { userService.getLoggedUserName() });
+
+		if (!checkPermissionForBudget(budgetRepository.findOneById(budgetId), userService.getLoggedUserName(),
+				PermissionType.VIEW)) {
+			throw new BudgetForbiddenAccessException(budgetId);
+		} else {
+			Set<Income> incomes = budgetRepository.findOneById(budgetId).getIncomes();
+			List<IncomeType> allIncomeTypes = new ArrayList<IncomeType>(Arrays.asList(IncomeType.values()));
+
+			for (IncomeType incomeType : allIncomeTypes) {
+				Double sumIncomesOfType = incomes.stream().filter(type -> type.getIncomeType().equals(incomeType))
+						.filter(date -> date.getDateTime().after(dateFrom) && date.getDateTime().before(dateTo))
+						.mapToDouble(item -> item.getAmount()).sum();
+
+				allSumsIncomesPerType.put(incomeType, sumIncomesOfType);
+			}
+			return allSumsIncomesPerType;
+		}
 	}
 
 }
