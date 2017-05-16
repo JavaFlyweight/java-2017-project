187ee6d605a30c178eafefb8b2eb120dbff6dceb
MAKOLAB-NET\mateusz.guzewski <mateusz.guzewski@makolab.com>
2017-05-16 18:02:56 +0200

    Disable autoconfigure for mongo.

--- budget-webapp/src/main/java/com/example/java/webapp/BudgetWebappApplication.java
+++ budget-webapp/src/main/java/com/example/java/webapp/BudgetWebappApplication.java
@@ -3,18 +3,20 @@ package com.example.java.webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.example.java"})
-@EnableMongoRepositories(basePackages = {"com.example.java.repository"})
+@SpringBootApplication(exclude = { MongoAutoConfiguration.class, MongoDataAutoConfiguration.class })
+@ComponentScan(basePackages = { "com.example.java" })
+@EnableMongoRepositories(basePackages = { "com.example.java.repository" })
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 public class BudgetWebappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BudgetWebappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetWebappApplication.class, args);
+    }
 }
