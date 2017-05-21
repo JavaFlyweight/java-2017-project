e212f6e2b03b40a268e4c5115a9fdbfff18e0747
MAKOLAB-NET\mateusz.guzewski <mateusz.guzewski@makolab.com>
2017-05-21 17:18:00 +0200

    Change mongoUri in application.properties.

--- budget-webapp/src/main/java/com/example/java/webapp/BudgetWebappApplication.java
+++ budget-webapp/src/main/java/com/example/java/webapp/BudgetWebappApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -12,7 +11,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.example.java" })
 @EnableMongoRepositories(basePackages = { "com.example.java.repository" })
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class, MongoAutoConfiguration.class })
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 public class BudgetWebappApplication {
 
     public static void main(String[] args) {
