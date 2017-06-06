a1539337c3967fe1409eaebef1e885cc9c0bbf8f
MAKOLAB-NET\mateusz.guzewski <mateusz.guzewski@makolab.com>
2017-06-06 15:47:10 +0200

    Disable spring boot auto configuration.

--- budget-repository/src/main/java/com/example/java/repository/BudgetRepository.java
+++ budget-repository/src/main/java/com/example/java/repository/BudgetRepository.java
@@ -1,9 +1,12 @@
 package com.example.java.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
 import com.example.java.domain.model.Budget;
 import com.example.java.repository.customs.BudgetRepositoryCustom;
 
+@Repository
 public interface BudgetRepository extends MongoRepository<Budget, String>, BudgetRepositoryCustom {
-	
+
 }
--- budget-webapp/src/main/java/com/example/java/webapp/BudgetWebappApplication.java
+++ budget-webapp/src/main/java/com/example/java/webapp/BudgetWebappApplication.java
@@ -1,17 +1,14 @@
 package com.example.java.webapp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "com.example.java" })
+@ComponentScan(basePackages = { "com.example.java.*" })
 @EnableMongoRepositories(basePackages = { "com.example.java.repository" })
-@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+//@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
 public class BudgetWebappApplication {
 
     public static void main(String[] args) {
